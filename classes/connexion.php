558c4753e44249955a6b576c547712d326c83c3b
otace1 <49515497+otace1@users.noreply.github.com>
2022-02-18 16:23:16 +0200

    Update connexion.php

--- classes/connexion.php
+++ classes/connexion.php
@@ -1,3 +1,3 @@
 <?php
-	$connexion = new PDO('mysql:host=localhost:3306;dbname=admin_malabar;charset=utf8','admin_malabar', 'NKTLmPrVgp_eerjut@kdmkkdiio1m');
+	$connexion = new PDO('mysql:host=localhost;dbname=admin_malabar;charset=utf8','admin_malabar', 'NKTLmPrVgp_eerjut@kdmkkdiio1m');
 ?>
