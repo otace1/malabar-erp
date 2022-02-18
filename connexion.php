2967c46a60193a65018ccc75e74d2ebc8aaabb44
otace1 <49515497+otace1@users.noreply.github.com>
2022-02-18 14:16:45 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,3 +1,3 @@
 <?php
-	$connexion = new PDO('mysql:host=private-db-mysql-fra1-03079-do-user-4618658-0.b.db.ondigitalocean.com:25060;dbname=malabar_db;charset=utf8','malabar_erp', 'o7fUznOicTpBePF9');
+	$connexion = new PDO('mysql:host=localhost;dbname=admin_malabar;charset=utf8','admin_malabar', 'NKTLmPrVgp_eerjut@kdmkkdiio1m');
 ?>
