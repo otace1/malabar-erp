db2dd7804bccb813fd4c660c3c36c99410956714
BeleJ94 <99182812+BeleJ94@users.noreply.github.com>
2022-02-22 10:34:03 +0200

    Add files via upload
    Modification status Amicongo

--- pages/exportExcelPopUp.php
+++ pages/exportExcelPopUp.php
@@ -676,14 +676,14 @@ while ($reponseModeTransport = $requeteModeTransport-> fetch()) {
 													, '') AS klsa_status,
 													IF(d.id_mod_trans='1' AND d.id_mod_lic='2', 
 														IF(d.bond_warehouse='LUBUMBASHI',
-															IF(d.warehouse_arriv IS NOT NULL AND d.warehouse_arriv IS NOT NULL, 'DISPATCHED FROM AMICONGO', 
+															IF(d.warehouse_arriv IS NOT NULL AND d.warehouse_dep IS NOT NULL, 'DISPATCHED FROM AMICONGO', 
 																IF(d.warehouse_arriv IS NOT NULL, 'ARRIVED AT AMICONGO', '')
 																)
 															,'')
 														,'') AS amicongo_status,
 													IF(d.id_mod_trans='1' AND d.id_mod_lic='2', 
 														IF(d.bond_warehouse='KOLWEZI',
-															IF(d.warehouse_arriv IS NOT NULL AND d.warehouse_arriv IS NOT NULL, 'DISPATCHED FROM WAREHOUSE', 
+															IF(d.warehouse_arriv IS NOT NULL AND d.warehouse_dep IS NOT NULL, 'DISPATCHED FROM WAREHOUSE', 
 																IF(d.warehouse_arriv IS NOT NULL, 'ARRIVED AT WAREHOUSE', '')
 																)
 															,'')
