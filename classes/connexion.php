bacfebffc9dd31e6ff6c6ddd0f2b1f40a91c4124
BeleJ94 <99182812+BeleJ94@users.noreply.github.com>
2022-09-20 22:22:53 +0200

    Update connexion.php

--- classes/connexion.php
+++ classes/connexion.php
@@ -1,4 +1,25 @@
 <?php
 
-	$connexion = new PDO('mysql:host=localhost;dbname=admin_malabar;charset=utf8','admin_malabar', 'NKTLmPrVgp_eerjut@kdmkkdiio1m');
+	//$connexion = new PDO('mysql:host=localhost;dbname=admin_malabar;charset=utf8','admin_malabar', 'NKTLmPrVgp_eerjut@kdmkkdiio1m');
+$servername = "db-mysql-sfo2-99144-do-user-4618658-0.b.db.ondigitalocean.com";
+$username = "mrdc_db_user";
+$password = "AVNS__sws1ImaA17TOAKgSPg";
+$options = array(
+	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+	PDO::MYSQL_ATTR_SSL_CA => 'classes/ca-certificate (4).crt',
+	PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+);
+
+try {
+    $connexion = new PDO("mysql:host=$servername;port=25060;dbname=admin_malabar", $username, $password, $options);
+
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+        //var_dump($conn->query("SHOW STATUS LIKE 'Ssl_cipher';")->fetchAll());
+        //$conn = null;
+}
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+}
 ?>
