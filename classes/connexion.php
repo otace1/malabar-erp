e576d061d63b6b9fbc9c064190b63663fddfe4c1
BeleJ94 <99182812+BeleJ94@users.noreply.github.com>
2022-09-20 20:35:14 +0200

    Update connexion.php

--- classes/connexion.php
+++ classes/connexion.php
@@ -1,3 +1,4 @@
 <?php
-	$connexion = new PDO('mysql:host=localhost;dbname=admin_malabar;charset=utf8','admin_malabar', 'NKTLmPrVgp_eerjut@kdmkkdiio1m');
+
+	$connexion = new PDO('mysql:host=tc172425-001.eu.clouddb.ovh.net:35512;dbname=admin_malabar;charset=utf8','admin_malabar', '8SW2sd6V9ocg');
 ?>
