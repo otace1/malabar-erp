c00fc9d6403be23a26ac666259e083f1b92e8984
BeleJ94 <99182812+BeleJ94@users.noreply.github.com>
2022-09-21 00:09:33 +0200

    Update connexion.php

--- classes/connexion.php
+++ classes/connexion.php
@@ -4,6 +4,7 @@
 $servername = "db-mysql-sfo2-99144-do-user-4618658-0.b.db.ondigitalocean.com";
 $username = "mrdc_db_user";
 $password = "AVNS__sws1ImaA17TOAKgSPg";
+$dbname = "admin_malabar";
 $options = array(
 	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 	PDO::MYSQL_ATTR_SSL_CA => 'ca-certificate (4).crt',
@@ -11,10 +12,10 @@ $options = array(
 );
 
 try {
-    $connexion = new PDO("mysql:host=$servername;port=25060;dbname=admin_malabar", $username, $password, $options);
+    $connexion = new PDO("mysql:host=$servername;port=25060;dbname=$dbname", $username, $password, $options);
 
     $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
+    //echo "Connected successfully"; 
         //var_dump($conn->query("SHOW STATUS LIKE 'Ssl_cipher';")->fetchAll());
         //$conn = null;
 }
