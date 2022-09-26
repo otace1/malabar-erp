4edd146321b274c3c01eb0c97aa7ccdc61054ea4
BeleJ94 <99182812+BeleJ94@users.noreply.github.com>
2022-09-26 11:28:18 +0200

    Add files via upload

--- pages/exportExcel2.php
+++ pages/exportExcel2.php
@@ -67,7 +67,7 @@ $requeteModeTransport-> execute(array($_GET['id_cli']));
 while ($reponseModeTransport = $requeteModeTransport-> fetch()) {
 
 	//Pour EXPORT créer des tableurs par licence et modes de transport
-	if ($_GET['id_mod_trac'] == '1' && $_GET['id_march'] != '18') {
+	if ($_GET['id_mod_trac'] == '1' && $_GET['id_march'] != '18' && $_GET['id_cli'] != '876' && $_GET['id_cli'] != '878') {
 		
 		//--- Recuperation d'années -------
 		if(isset($_GET['annee']) && ($_GET['annee']!='')){
