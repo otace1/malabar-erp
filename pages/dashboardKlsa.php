f63b0da7b0fca68d3d4993f9fca8322a3e453af9
BeleJ94 <99182812+BeleJ94@users.noreply.github.com>
2023-01-31 13:47:46 +0200

    Add files via upload

--- pages/dashboardKlsa.php
+++ pages/dashboardKlsa.php
@@ -8,6 +8,27 @@
 
   <div class="col-md-3 col-sm-6 col-12">
 
+    <div class="small-box bg-info">
+      <div class="inner">
+        <h3>
+          <?php echo $maClasse-> nbreDossierProcess($_GET['id_mod_trac'], $_GET['id_cli'], $_GET['id_mod_trans'], $_GET['commodity']);?>
+        </h3>
+
+        <p> FILES IN PROCESS </p>
+      </div>
+      <div class="icon">
+        <i class="fas fa-file"></i>
+      </div>
+      <a href="#" class="small-box-footer" onclick="window.open('popUpDossierDashboard.php?etat=FILES IN PROCESS&id_cli=<?php echo $_GET['id_cli'];?>&id_mod_lic=<?php echo $_GET['id_mod_trac'];?>&id_mod_trans=<?php echo $_GET['id_mod_trans'];?>&commodity=<?php echo $_GET['commodity'];?>','pop1','width=1500,height=900');">
+        More info <i class="fas fa-arrow-circle-right"></i>
+      </a>
+    </div>
+
+    <!-- /.info-box -->
+  </div>
+
+  <div class="col-md-3 col-sm-6 col-12">
+
     <div class="small-box bg-primary">
       <div class="inner">
         <h3>
