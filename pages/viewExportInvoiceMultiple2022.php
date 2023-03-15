cbd07187798242d6757041788b162fe13d9fad81
BeleJ94 <99182812+BeleJ94@users.noreply.github.com>
2023-03-15 15:13:33 +0200

    Add files via upload

--- pages/viewExportInvoiceMultiple2022.php
+++ pages/viewExportInvoiceMultiple2022.php
@@ -306,6 +306,40 @@ if ($facture['id_cli']==864 || $facture['id_cli']==878 || $facture['id_cli']==87
 					&nbsp;LUBUMBASHI, R.D.CONGO</td>
 					<td width="10%"></td>
 				</tr>';
+}else if ($facture['id_cli']==916) {
+	$banque = '<tr>
+					<td width="10%" style="border-top: 1px solid black; border-left: 1px solid black; text-align: left;  font-size: 7px;">&nbsp;INTITULE</td>
+					<td width="35%" style="border-top: 1px solid black; border-right: 1px solid black;  font-size: 7px;">&nbsp;MALABAR RDC SARL</td>
+					<td width="10%"></td>
+				</tr>
+				<tr>
+					<td width="10%" style="border-left: 1px solid black;  font-size: 7px;">&nbsp;N.COMPTE</td>
+					<td width="35%" style="border-right: 1px solid black; text-align: left;  font-size: 7px;">&nbsp;00026  00004  35200002629  39</td>
+					<td width="10%"></td>
+				</tr>
+				<tr>
+					<td width="10%" style="border-left: 1px solid black; text-align: left;  font-size: 7px;">&nbsp;SWIFT</td>
+					<td width="35%" style="border-right: 1px solid black; text-align: left;  font-size: 7px;">&nbsp;ECOCCDKI</td>
+					<td width="10%"></td>
+				</tr>
+				<tr>
+					<td width="10%" style=" border-left: 1px solid black; text-align: left;  font-size: 7px;">&nbsp;BANQUE</td>
+					<td width="35%" style=" border-right: 1px solid black; text-align: left;  font-size: 7px;">&nbsp;ECOBANK DRC</td>
+					<td width="10%"></td>
+				</tr>
+				<tr>
+					<td width="10%" style="border-left: 1px solid black;  font-size: 7px;">&nbsp;N.COMPTE</td>
+					<td width="35%" style="border-right: 1px solid black; text-align: left;  font-size: 7px;">&nbsp;00011-00130-00001020614-41</td>
+					<td width="10%"></td>
+				</tr>
+				<tr>
+					<td width="10%" style="border-bottom: 1px solid black; border-left: 1px solid black; text-align: left;  font-size: 7px;">&nbsp;SWIFT<br>
+					&nbsp;BANQUE</td>
+					<td width="35%" style="border-bottom: 1px solid black; border-right: 1px solid black; text-align: left;  font-size: 7px;">&nbsp;BCDCCDKI<br>
+					&nbsp;EQUITY BCDC CONGO SA<br>
+					&nbsp;LUBUMBASHI, R.D.CONGO</td>
+					<td width="10%"></td>
+				</tr>';
 }
 
 $tbl = <<<EOD
