c4836cf2c42ce2239f92085a3cad9ea525d38417
BeleJ94 <99182812+BeleJ94@users.noreply.github.com>
2023-08-18 15:49:02 +0200

    Add files via upload

--- pages/factureService.php
+++ pages/factureService.php
@@ -1,7 +1,7 @@
 <?php
   include("tetePopCDN.php");
   include("menuHaut.php");
-  // include("menuGauche.php");
+  include("menuGauche.php");
 
    if( isset($_POST['creerFactureDossier']) ){
     ?>
