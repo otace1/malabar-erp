3ca9a3db3d1fd008406ae2a9283dab4eac79041b
BeleJ94 <99182812+BeleJ94@users.noreply.github.com>
2024-01-19 09:12:35 +0200

    Add files via upload

--- pages/viewExportInvoiceMultiple2022.php
+++ pages/viewExportInvoiceMultiple2022.php
@@ -61,7 +61,7 @@ if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
 
 
 // set font
-$pdf->SetFont('helvetica', 'N', 8);
+$pdf->SetFont('helvetica', 'N', 7);
 // $pdf->SetFont('times', 'N', 8);
 // $pdf->SetFont('courier', 'N', 8);
 $pdf->setPrintHeader(false);
