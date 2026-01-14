1459a26c93e73499607a8ee3f788b9b924620fee
BeleJ94 <99182812+BeleJ94@users.noreply.github.com>
2026-01-14 17:55:45 +0200

    Add files via upload

--- pages/viewAcidImportInvoiceMultiple2022_14012026.php
+++ pages/viewAcidImportInvoiceMultiple2022_14012026.php
@@ -134,8 +134,6 @@ $operational_cost = $maClasse-> getDetailFactureExportMultiple($_GET['ref_fact']
 $service_fee = $maClasse-> getDetailFactureExportMultiple($_GET['ref_fact'], '4');
 
 $totalAll = $maClasse-> getTotalFactureExportSingle($_GET['ref_fact']);
-$totalAll_A = $maClasse-> getTotalFactureExportSingle_A($_GET['ref_fact']);
-$totalAll_B = $maClasse-> getTotalFactureExportSingle_B($_GET['ref_fact']);
 
 $total = $maClasse-> getTotalForFacturePartielle($_GET['ref_fact']);
 $arsp = $maClasse-> getARSPForFacturePartielle($_GET['ref_fact']);
@@ -216,7 +214,7 @@ $tbl = <<<EOD
 		<br>
 		<br>
 		<tr>
-			<td width="45%" style="text-align: center; border: 0.3px solid black; font-weight: bold; font-size: 12px;">DEBIT NOTE</td>
+			<td width="45%" style="text-align: center; border: 0.3px solid black; font-weight: bold; font-size: 12px;">FACTURE</td>
 		</tr>
 		<br>
 		<tr>
@@ -226,11 +224,10 @@ $tbl = <<<EOD
 			<br>No.RCCM: $rccm_cli
 			<br>No.NIF.: $nif_cli
 			<br>No.IDN.: $id_nat
-			<br>No.IMPORT/EXPORT: $num_imp_exp
-			<br>No.TVA: $num_tva</td>
+			<br>No.IMPORT/EXPORT: $num_imp_exp</td>
 			<td width="15%" style="text-align: center;"></td>
-			<td width="18%" style="text-align: left; border: 0.3px solid black; font-size: 7px;">&nbsp;N.DEBIT NOTE</td>
-			<td width="22%" style="text-align: center; border: 0.3px solid black; font-weight: bold; font-size: 7px;">ND-$ref_fact</td>
+			<td width="18%" style="text-align: left; border: 0.3px solid black; font-size: 7px;">&nbsp;N.FACTURE</td>
+			<td width="22%" style="text-align: center; border: 0.3px solid black; font-weight: bold; font-size: 7px;">$ref_fact</td>
 		</tr>
 		<tr>
 			<td width="15%" style="text-align: center;"></td>
@@ -241,33 +238,25 @@ $tbl = <<<EOD
 		<tr>
 			<td width="15%" style="text-align: left; "></td>
 			<td width="40%"rowspan="2" colspan="2" style="text-align: left; border: 0.3px solid black; font-size: 7px;">&nbsp;<u>Dossier(s):</u> <br>
-			<b>$liste_dossiers</b></td>
+			<b><span>$liste_dossiers</span></b></td>
 		</tr>
 		<tr>
 			<td width="15%" style="text-align: left; "></td>
 		</tr>
 		<tr>
 			<td width="15%" style="text-align: left; "></td>
-			<td width="18%" style="text-align: left; border: 0.3px solid black; font-size: 7px;">&nbsp;Nombre de Dossier(s): </td>
+			<td width="18%" style="text-align: left; border: 0.3px solid black; font-size: 7px;">&nbsp;Rate: </td>
+			<td width="22%" style="text-align: center; border: 0.3px solid black; font-size: 7px; font-weight: bold;">$roe_decl</td>
+		</tr>
+		<tr>
+			<td width="15%" style="text-align: left; "></td>
+			<td width="18%" style="text-align: left; border: 0.3px solid black; font-size: 7px;">&nbsp;Nombre de Trucks: </td>
 			<td width="22%" style="text-align: center; border: 0.3px solid black; font-size: 7px; font-weight: bold;">$nbre_dos</td>
 		</tr>
 		<tr>
 			<td width="15%" style="text-align: left; "></td>
 			<td width="18%" style="text-align: left;"></td>
 			<td width="22%" style="text-align: center; font-weight: bold;"></td>
-		</tr>
-		<tr>
-			<td width="15%" style="text-align: left; "></td>
-			<td width="18%" style="text-align: left;"></td>
-			<td width="22%" style="text-align: center; font-weight: bold;"></td>
-		</tr>
-
-		<tr>
-			<td width="100%"><br></td>
-		</tr>
-
-		<tr>
-			<td width="100%" style="font-weight: bold; text-align: center;">REIMBURSABLE CHARGES</td>
 		</tr>
 
 		<tr>
@@ -287,9 +276,6 @@ $tbl = <<<EOD
 		</tr>
 		$taxe
 		<tr>
-			<td colspan="4"></td>
-		</tr>
-		<tr>
 			<td width="100%" style="font-weight: bold; border: 1px solid black;">&nbsp;<u>OTHER CHARGES / AUTRES FRAIS </u></td>
 		</tr>
 		<tr>
@@ -301,139 +287,6 @@ $tbl = <<<EOD
 			<td width="11.5%" style="font-weight: bold; border: 1px solid black; background-color: rgb(220,220,220);text-align: center;">TOTAL  EN USD</td>
 		</tr>
 		$autres_charges
-		<tr>
-			<td width="100%"></td>
-		</tr>
-		$totalAll_A
-		<tr>
-			<td><br></td>
-		</tr>
-		<tr>
-			<td width="45%" style=" font-size: 6.5px;">
-			VEUILLEZ TROUVER CI-DESSOUS LES DETAILS DE NOTRE COMPTE BANCAIRE
-			</td>
-			<td width="55%" rowspan="8" style="text-align: center;">$sceau</td>
-		</tr>
-		<tr>
-			<td width="45%" style=" font-size: 6.5px;">
-			</td>
-		</tr>
-		<tr>
-			<td width="10%" style="border-top: 1px solid black; border-left: 1px solid black; text-align: left;  font-size: 7px;">&nbsp;INTITULE</td>
-			<td width="35%" style="border-top: 1px solid black; border-right: 1px solid black;  font-size: 7px;">&nbsp;MALABAR RDC SARL</td>
-		</tr>
-		<tr>
-			<td width="10%" style="border-left: 1px solid black;  font-size: 7px;">&nbsp;N.COMPTE</td>
-			<td width="35%" style="border-right: 1px solid black; text-align: left;  font-size: 7px;">&nbsp;00011-00130-00001020614-41</td>
-		</tr>
-		<tr>
-			<td width="10%" style="border-left: 1px solid black; text-align: left;  font-size: 7px;">&nbsp;SWIFT</td>
-			<td width="35%" style="border-right: 1px solid black; text-align: left;  font-size: 7px;">&nbsp;BCDCCDKI</td>
-		</tr>
-		<tr>
-			<td width="10%" style="border-bottom: 1px solid black; border-left: 1px solid black; text-align: left;  font-size: 7px;">&nbsp;BANQUE</td>
-			<td width="35%" style="border-bottom: 1px solid black; border-right: 1px solid black; text-align: left;  font-size: 7px;">&nbsp;BCDC LUBUMBASHI		
-					, R.D. CONGO</td>
-		</tr>
-		<tr>
-			<td width="100%"></td>
-		</tr>
-		<tr>
-			<td width="65%" style=" font-size: 7px;">
-			LE PAIEMENT DOIT S'EFFECTUER ENDEANS 7 JOURS
-			</td>
-		</tr>
-		<tr>
-			<td width="100%"></td>
-		</tr>
-		<tr>
-			<td width="100%" style="border: 1px solid black; text-align: center; font-size: 7px;">Thank you for you business!</td>
-		</tr>
-	</table>
-	</bodystyle="font-weight: bold;">
-	</html>
-        
-EOD;
-$pdf->writeHTML($tbl, true, false, false, false, '');
-
-// add a page
-$pdf->AddPage('P', 'A4');
-
-$tbl = <<<EOD
-    <html>
-    <head>
-        <meta http-equiv = " content-type " content = " text/html; charset=utf-8" />
-    </head>
-    <body style="font-weight: bold;" style="">
-	<table>
-		<tr>
-			<td width="45%" style="text-align: center;">$logo</td>
-			<td width="55%" style="text-align: right; font-size: 5px;">
-			No. 1068, Avenue Ruwe, Quartier Makutano, <br>
-			Lubumbashi, DRC<br>
-			RCCM: 13-B-1122, ID NAT. 6-9-N91867E<br>
-			NIF : A 1309334 L<br>
-			VAT Ref # 145/DGI/DGE/INF/BN/TVA/2020<br>
-			Capital Social : 45.000.000 FC
-			</td>
-		</tr>
-		<br>
-		<br>
-		<tr>
-			<td width="45%" style="text-align: center; border: 0.3px solid black; font-weight: bold; font-size: 12px;">FACTURE</td>
-		</tr>
-		<br>
-		<tr>
-			<td width="45%" rowspan="7" style="text-align: left; border: 0.3px solid black; font-size: 7px;"><span><u>CLIENT</u></span>
-			<br><b><font size="8px">$nom_complet</font> </b>
-			<br>$adresse_client
-			<br>No.RCCM: $rccm_cli
-			<br>No.NIF.: $nif_cli
-			<br>No.IDN.: $id_nat
-			<br>No.IMPORT/EXPORT: $num_imp_exp
-			<br>No.TVA: $num_tva</td>
-			<td width="15%" style="text-align: center;"></td>
-			<td width="18%" style="text-align: left; border: 0.3px solid black; font-size: 7px;">&nbsp;N.FACTURE</td>
-			<td width="22%" style="text-align: center; border: 0.3px solid black; font-weight: bold; font-size: 7px;">$ref_fact</td>
-		</tr>
-		<tr>
-			<td width="15%" style="text-align: center;"></td>
-			<td width="18%" style="text-align: left; border: 0.3px solid black; font-size: 7px;">&nbsp;Date</td>
-			<td width="22%" style="text-align: center; border: 0.3px solid black; font-weight: bold; font-size: 7px;">$date_fact</td>
-		</tr>
-
-		<tr>
-			<td width="15%" style="text-align: left; "></td>
-			<td width="40%"rowspan="2" colspan="2" style="text-align: left; border: 0.3px solid black; font-size: 7px;">&nbsp;<u>Dossier(s):</u> <br>
-			<b>$liste_dossiers</b></td>
-		</tr>
-		<tr>
-			<td width="15%" style="text-align: left; "></td>
-		</tr>
-		<tr>
-			<td width="15%" style="text-align: left; "></td>
-			<td width="18%" style="text-align: left; border: 0.3px solid black; font-size: 7px;">&nbsp;Nombre de Dossier(s): </td>
-			<td width="22%" style="text-align: center; border: 0.3px solid black; font-size: 7px; font-weight: bold;">$nbre_dos</td>
-		</tr>
-		<tr>
-			<td width="15%" style="text-align: left; "></td>
-			<td width="18%" style="text-align: left;"></td>
-			<td width="22%" style="text-align: center; font-weight: bold;"></td>
-		</tr>
-		<tr>
-			<td width="15%" style="text-align: left; "></td>
-			<td width="18%" style="text-align: left;"></td>
-			<td width="22%" style="text-align: center; font-weight: bold;"></td>
-		</tr>
-
-		<tr>
-			<td width="100%"><br></td>
-		</tr>
-
-		<tr>
-			<td width="100%"><br></td>
-		</tr>
-
 		<tr>
 			<td width="100%" style="font-weight: bold; border: 1px solid black;">&nbsp;<u>OPERATIONAL COSTS / COUT OPERATIONEL</u></td>
 		</tr>
@@ -447,9 +300,6 @@ $tbl = <<<EOD
 		</tr>
 		$operational_cost
 		<tr>
-			<td width="100%"></td>
-		</tr>
-		<tr>
 			<td width="100%" style="font-weight: bold; border: 1px solid black;">&nbsp;<u>SERVICE FEE / SERVICES</u></td>
 		</tr>
 		<tr>
@@ -461,10 +311,7 @@ $tbl = <<<EOD
 			<td width="11.5%" style="font-weight: bold; border: 1px solid black; background-color: rgb(220,220,220);text-align: center;">TOTAL  EN USD</td>
 		</tr>
 		$service_fee
-		<tr>
-			<td width="100%"></td>
-		</tr>
-		$totalAll_B
+		$totalAll
 		<tr>
 			<td><br></td>
 		</tr>
@@ -495,6 +342,7 @@ $tbl = <<<EOD
 			<td width="35%" style="border-bottom: 1px solid black; border-right: 1px solid black; text-align: left;  font-size: 7px;">&nbsp;BCDC LUBUMBASHI		
 					, R.D. CONGO</td>
 		</tr>
+		
 		<tr>
 			<td width="100%"></td>
 		</tr>
